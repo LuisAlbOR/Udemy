b86ee832f3b5b3576a4c0419d05b2591c3f13627
GuichoMaster <luiaballz.laor@gmail.com>
2023-06-01 12:25:09 -0600

    Se crea el proyecto para el curso

--- Java/JavaDesdeCero/src/PrimitivoFloat.java
+++ Java/JavaDesdeCero/src/PrimitivoFloat.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class PrimitivoFloat {
+public class PrimitivoFloat {
+    public static void main(String[] args) {
+        float realFloat = 1.0F;
+        System.out.println("numeroFloat = " + realFloat);
+    
+        float realFloatExponente = 2.13e5F;
+        System.out.println("realFloatExponente = " + realFloatExponente);
+    
+        float realFloatExponenteNegativo = 0.00000000015F;//1.5e-10F;
+        System.out.println("realFloatExponenteNegativo = " + realFloatExponenteNegativo);
+        System.out.println("Float corresponde en byte: " + Float.BYTES);
+        System.out.println("Float corresponde en bites: " + Float.SIZE);
+        System.out.println("El valor minimo de float: " + Float.MIN_VALUE);
+        System.out.println("El valor maximo de float: " + Float.MAX_VALUE);
+
+        double realDouble = 3.4028235E38;
+        System.out.println("realDouble = " + realDouble);
+        System.out.println("Double corresponde en byte: " + Double.BYTES);
+        System.out.println("Double corresponde en bites: " + Double.SIZE);
+        System.out.println("El valor minimo de double: " + Double.MIN_VALUE);
+        System.out.println("El valor maximo de double: " + Double.MAX_VALUE);
+
+
+    }
 }
--- Java/JavaDesdeCero/src/Primitivos.java
+++ Java/JavaDesdeCero/src/Primitivos.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class Primitivos {
+public class Primitivos {
+    public static void main(String[] argos){
+        byte numeroByte = 8;
+        System.out.println("numeroByte = " + numeroByte);
+        System.out.println("Tipo Byte corresponde a: " + Byte.BYTES);
+        System.out.println("Tipo Byte corresponde a: " + Byte.SIZE);
+        System.out.println("El valor minimo de un byte: " + Byte.MIN_VALUE);
+        System.out.println("El valor maximo de un byte: " + Byte.MAX_VALUE);
+
+        short numeroShort = 10000;
+        System.out.println("numeroShort: " + numeroShort);
+        System.out.println("Tipo short corresponde en byte: " + Short.BYTES);
+        System.out.println("Tipo short corresponde en bite: " + Short.SIZE);
+        System.out.println("El valor minimo de un short: "  + Short.MIN_VALUE);
+        System.out.println("El valor maximo de un short: " + Short.MAX_VALUE);
+
+        int numeroInt = 32768;
+        System.out.println("numeroInt = " + numeroInt);
+        System.out.println("Tipo int corresponde en byte: " + Integer.BYTES);
+        System.out.println("Tipo int corresponde en bite: " + Integer.SIZE);
+        System.out.println("El valor minimo de un int: "  + Integer.MIN_VALUE);
+        System.out.println("El valor maximo de un int: " + Integer.MAX_VALUE);
+
+        long numeroLong = 2147483648L;
+        System.out.println("numeroLong = " + numeroLong);
+        System.out.println("Tipo long corresponde en byte: " + Long.BYTES);
+        System.out.println("Tipo long corresponde en bite: " + Long.SIZE);
+        System.out.println("El valor minimo de un long: "  + Long.MIN_VALUE);
+        System.out.println("El valor maximo de un long: " + Long.MAX_VALUE);
+
+        //El valor por defecto de cualquier dato primitivo numérico es 0
+    }
 }
