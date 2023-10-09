a90f8963992477457052d11c2d97178d40e28280
GuichoMaster <luiaballz.laor@gmail.com>
2023-10-08 22:39:06 -0600

    Se suben cambios de directorio

---
+++ Python/juego_python/Fundamentos_Pygame/tiempo.py
@@ -0,0 +1,68 @@
+from operator import pos
+
+import pygame
+import sys
+from pygame.locals import *
+from random import randint
+
+pygame.init()
+
+ventana = pygame.display.set_mode((500,400))
+pygame.display.set_caption("Titulo de la ventana")
+colorFondo = (100,150,70)
+
+#Definiendo colores de las figuras
+colorCuadroUno = (255,255,255)
+colorCuadroDos = (0,0,0)
+colorTexto = (255,255,255)
+
+#Variables
+#velocidad = 15
+direccion = True
+posicionXCuadroUno,posicionYCuadroUno = randint(1, 500),randint(1, 360)
+posicionXCuadroDos, posicionYCuadroDos = randint(1, 500),randint(1, 360)
+lado = 40
+cadena = "Tiempo"
+tamanno = 40
+tipoFuente = "Verdana"
+
+#Preparacion de funte / texto
+fuente = pygame.font.SysFont(tipoFuente,tamanno)
+texto = fuente.render(cadena,True,colorTexto)
+
+while True:
+    ventana.fill(color=colorFondo)
+
+    #Mostrar texto
+    ventana.blit(texto,(10,50))
+
+    #Tiempo
+    tiempo = pygame.time.get_ticks() / 1000
+    cadena = "Tiempo: " + str(tiempo)
+    texto = fuente.render(cadena,True,colorTexto)
+
+    cuadroUno = pygame.draw.rect(ventana,colorCuadroUno,(posicionXCuadroUno,posicionYCuadroUno,40,40))
+    cuadroDos = pygame.draw.rect(ventana,colorCuadroDos,(posicionXCuadroDos,posicionYCuadroDos,40,40))
+
+
+    #Detectar movimientos con el raton
+    posicionXCuadroUno,posicionYCuadroUno = pygame.mouse.get_pos()
+    posicionXCuadroUno = posicionXCuadroUno - (lado/2)
+    posicionYCuadroUno = posicionYCuadroUno - (lado/2)
+
+    #Detectar colisiones
+    if cuadroUno.colliderect(cuadroDos):
+        #print(f"colision en {posicionXCuadroUno} : {posicionYCuadroUno}")
+        #cadena = f"colision en {posicionXCuadroUno} : {posicionYCuadroUno}"
+        #texto = fuente.render(cadena, True, colorTexto)
+
+        posicionXCuadroDos, posicionYCuadroDos = randint(1, 460),randint(1, 360)
+        cuadroDos.left = posicionXCuadroDos - (lado/2)
+        cuadroDos.top = posicionYCuadroDos - (lado/2)
+
+    for evento in pygame.event.get():
+        if evento.type == QUIT:
+            pygame.quit()
+            sys.exit()
+
+    pygame.display.update()
